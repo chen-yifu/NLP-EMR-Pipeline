8c440497f056a8c98a48c5fc0497474da3176633
Lucy <hao.lucyy@gmail.com>
2021-02-23 18:23:54 -0700

    abstracted to emr_pipeline.py file

---
+++ pipeline/emr_pipeline.py
@@ -0,0 +1,25 @@
+import time
+from pipeline.operative_pipeline.operative_pipeline import run_operative_pipeline
+from pipeline.pathology_pipeline.pathology_pipeline import run_pathology_pipeline
+
+
+def run_pipeline():
+    """
+    start: int, end: int, skip: List[int]
+
+    :param start:
+    :param end:
+    :param skip:
+    """
+    start = time.time()
+    run_pathology_pipeline(101, 156, skip=[140])
+    end = time.time()
+    pathology = end - start
+
+    start = time.time()
+    run_operative_pipeline(start=1, end=48, skip=[22, 43])
+    end = time.time()
+    op = end - start
+
+    print("Pathology Time: {}".format(pathology))
+    print("Operative Time: {}".format(op))
--- pipeline/main.py
+++ pipeline/main.py
@@ -1,31 +1,3 @@
-import time
-from typing import List
-
-from pipeline.check_report import *
-from pipeline.operative_pipeline.operative_pipeline import run_operative_pipeline
-from pipeline.pathology_pipeline.pathology_pipeline import run_pathology_pipeline
-
-
-def run_pipeline():
-    """
-    start: int, end: int, skip: List[int]
-
-    :param start:
-    :param end:
-    :param skip:
-    """
-    start = time.time()
-    run_pathology_pipeline(101, 156, skip=[140])
-    end = time.time()
-    pathology = end - start
-
-    start = time.time()
-    run_operative_pipeline(start=1, end=48, skip=[22, 43])
-    end = time.time()
-    op = end - start
-
-    print("Pathology Time: {}".format(pathology))
-    print("Operative Time: {}".format(op))
-
+from pipeline.emr_pipeline import run_pipeline
 
 run_pipeline()
