d74748e15dfa728d110a51ddaee6f9e598483056
Lucy <hao.lucyy@gmail.com>
2021-02-26 21:06:35 -0700

    last commit b4 the weekend 😎

--- pipeline/util/regex_tools.py
+++ pipeline/util/regex_tools.py
@@ -1,7 +1,118 @@
 import re
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
-from pipeline.util.utils import capture_double_regex
+
+def regex_extract(regex: str, uncleaned_txt: str) -> list:
+    return re.findall(re.compile(regex), uncleaned_txt)
+
+
+def extract_section(regexs: List[Tuple[str, str]], uncleaned_txt: str) -> list:
+    """
+    General function that takes in a list of regex and returns the first one that returns a result
+
+    :param uncleaned_txt:
+    :param regexs:            list of tuple(regex,to_append) and the list should ne entered in priority
+    :return:
+    """
+    for regex, to_append in regexs:
+        extraction_result = regex_extract(regex, uncleaned_txt)
+        if len(extraction_result) != 0:
+            if to_append == "":
+                return extraction_result
+            result = to_append + extraction_result[0]
+            return [result]
+    return []
+
+
+def convert_to_regex(list_of_words: List[Union[str, List[str]]]) -> str:
+    """
+    Helper function to convert a string into regex
+
+    :param list_of_words:
+    :return:
+    """
+    result = ""
+    for word in list_of_words[:-1]:
+        if isinstance(word, list):
+            result += "".join([add_asterisk(or_word) + "|" for or_word in word])
+        elif word == " ":
+            result += " "
+        else:
+            result += add_asterisk(word) + ".*"
+
+    last_word = list_of_words[-1]
+    if isinstance(last_word, list):
+        result += "".join([add_asterisk(or_word) + "|" for or_word in last_word[:-1]]) + add_asterisk(last_word[-1])
+    else:
+        result += add_asterisk(last_word)
+    return result
+
+
+def add_asterisk(word: str) -> str:
+    """
+    Adds asterisk where appropriate
+    :param word:
+    :return:
+    """
+    letters = list(word)
+    result = ""
+    for index in range(len(letters) - 1):
+        curr = letters[index]
+        next = letters[index + 1]
+        if curr != " " and next != " ":
+            curr += " *"
+        result += curr
+    result += letters[-1]
+    if letters[0] == "-" and letters[1] == " ":
+        result = "-+ *" + result[2:]
+    elif letters[0] == " ":
+        result = " *" + result[1:]
+    elif letters[0] == "-":
+        result = "-+" + result[1:]
+    return re.sub(' +', ' ', result)
+
+
+def capture_laterality() -> str:
+    return "yeet"
+
+
+def synoptic_regex() -> str:
+    return "yeet"
+
+
+def capture_double_regex(starting_word: List[Union[str, List[str]]],
+                         ending_word: List[Union[str, List[str]]],
+                         capture_first_line: bool = False,
+                         capture_last_line: bool = False,
+                         ignore_capials: bool = True) -> str:
+    """
+    for spaces:
+    ["cat dog"],["fish"], True ->  r"(?i)c *a *t d *o *g(?P<capture>(?:(?!f *i *s *h.*)[\s\S])+)"
+    ["cat dog"],["fish"], True ->  r"(?i)c *a *t d *o *g.+(?P<capture>(?:(?!f *i *s *h)[\s\S])+)"
+    ["cat dog"],["fish"] ->        r"(?i)c *a *t d *o *g(?P<capture>(?:(?!f *i *s *h)[\s\S])+)"
+    ["cat"," ","dog"],["fish"] ->  r"(?i)c *a *t.* d *o *g(?P<capture>(?:(?!f *i * s *h)[\s\S])+)"
+    ["cat "," ","dog"],["fish"] -> r"(?i)c *a *t .* d *o *g(?P<capture>(?:(?!f *i * s *h)[\s\S])+)"
+    ["cat"," dog"],["fish"] ->     r"(?i)c *a *t.* *d *o *g(?P<capture>(?:(?!f *i * s *h)[\s\S])+)"
+
+    :param ignore_capials:          whether or not you want be capital sensitive
+    :param capture_last_line:       whether or not you want your regex to capture everything after the last word
+    :param capture_first_line:      whether or not you want your regex to capture everything after the first word
+    :param starting_word:           the first word to look for
+    :param ending_word:             the word to stop at
+    :return:
+    """
+    modified_starting_word = convert_to_regex(starting_word)
+    if capture_first_line:
+        modified_starting_word += ".+"
+    modified_ending_word = convert_to_regex(ending_word)
+    if capture_last_line:
+        modified_ending_word += ".*"
+    modified_regex = r"{capitalize}{starting_word}(?P<capture>(?:(?!{ending_word})[\s\S])+)".format(
+        capitalize="(?i)" if ignore_capials else "",
+        starting_word=modified_starting_word,
+        ending_word=modified_ending_word)
+    return modified_regex
+
 
 # regex patterns for operative reports
 # https://regex101.com/r/kEj3Fs/1
@@ -44,25 +155,3 @@ pathology_synoptic_regex = [(capture_double_regex(["Synoptic Report: "], ["- End
 
 export_operative_regex = [preoperative_rational_regex, operative_breast_regex, operative_axilla_regex]
 export_pathology_regex = [pathology_synoptic_regex]
-
-
-def regex_extract(regex: str, uncleaned_txt: str) -> list:
-    return re.findall(re.compile(regex), uncleaned_txt)
-
-
-def extract_section(regexs: List[Tuple[str, str]], uncleaned_txt: str) -> list:
-    """
-    General function that takes in a list of regex and returns the first one that returns a result
-
-    :param uncleaned_txt:
-    :param regexs:            list of tuple(regex,to_append) and the list should ne entered in priority
-    :return:
-    """
-    for regex, to_append in regexs:
-        extraction_result = regex_extract(regex, uncleaned_txt)
-        if len(extraction_result) != 0:
-            if to_append == "":
-                return extraction_result
-            result = to_append + extraction_result[0]
-            return [result]
-    return []
